92305dac69e9042aca23bf57e0c9582e3be6371d
akhilkommalap277 <siddarthrahul07@gmail.com>
2020-07-14 23:55:33 -0500

    Prototype Design Pattern

---
+++ src/main/java/PrototypeDesignPattern/Circle.java
@@ -0,0 +1,13 @@
+package PrototypeDesignPattern;
+
+public class Circle extends Shape{
+
+    public Circle(){
+        type = "Circle";
+    }
+
+    @Override
+    void draw() {
+        System.out.println("Circle ");
+    }
+}
